b19b4c8d019db68f921432a2693f6bb9f74f20ea
Axel Thieffry <axel.thieffry@gmail.com>
2018-09-27 13:56:19 +0200

    Update wideMetaProfile-methods.R

--- R/wideMetaProfile-methods.R
+++ R/wideMetaProfile-methods.R
@@ -55,30 +55,32 @@ setMethod("wideMetaProfile",
           signature(forward="BigWigFile_OR_RleList", reverse="BigWigFile_OR_RleList", sites="GenomicRanges"),
           function(sites, forward, reverse=NULL, upstream=100, downstream=100) {
               # Pre-checks
-              assertthat::assert_that(assertthat::are_equal(class(forward),
-                                    class(reverse)),
-                          all(width(sites) == 1),
-                          assertthat::is.count(upstream),
-                          assertthat::is.count(downstream))
+              assertthat::assert_that(assertthat::are_equal(class(forward), class(reverse)),
+                                      all(width(sites) == 1),
+                                      assertthat::is.count(upstream),
+                                      assertthat::is.count(downstream))
 
               # Expand and split
               sites <- promoters(sites, upstream=upstream, downstream=downstream)
-              strand(sites)[strand(sites) == "*"] <- "+"
+              if(any(strand(sites) == "*")) {
+                        message('Converting unstranded site(s) to + strand')
+                        strand(sites)[strand(sites) == "*"] <- "+"
+                        }
               sites_stranded <- split(sites, strand(sites))
 
               # Coverage in all directions
               sense_forward <- agnosticImport(signal=forward, sites=sites_stranded$`+`)
-              anti_forward <- agnosticImport(signal=reverse, sites=sites_stranded$`+`)
+              anti_forward  <- agnosticImport(signal=reverse, sites=sites_stranded$`+`)
               sense_reverse <- agnosticImport(signal=reverse, sites=sites_stranded$`-`)
-              anti_reverse <- agnosticImport(signal=forward, sites=sites_stranded$`-`)
+              anti_reverse  <- agnosticImport(signal=forward, sites=sites_stranded$`-`)
 
-              # Reverse minues
+              # Reverse minus
               sense_reverse <- revElements(x=sense_reverse, i=seq_along(sense_reverse))
-              anti_reverse <- revElements(x=anti_reverse, i=seq_along(sense_reverse))
+              anti_reverse  <- revElements(x=anti_reverse, i=seq_along(anti_reverse))
 
               # Combine matrices
               mat_sense <- do.call(rbind, c(sense_forward, sense_reverse))
-              mat_anti <- do.call(rbind, c(anti_forward, anti_reverse))
+              mat_anti  <- do.call(rbind, c(anti_forward, anti_reverse))
 
               # Name
               colnames(mat_sense) <- colnames(mat_anti) <- c(-upstream:-1, 1:downstream)
